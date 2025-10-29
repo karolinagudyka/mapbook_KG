c705cf22ffba9373e2df87870dd990c0ba100b17
karolinagudyka <u56121@student.wat.edu.pl>
2025-10-29 09:10:26 +0100

    Wip: added for loop with users

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+zmienna_1:str = "Karolina"
+zmienna_2:str = "Tomek"
+zmienna_3:str = "Jacek"
+zmienna_4:str = "Piotr"
+
+
+#przerób istniejący kod żeby użytkownicy wyśweitlali się w pętli
+uzytkownicy:list = [zmienna_1, zmienna_2, zmienna_3, zmienna_4]
+for uzytkownik in uzytkownicy:
+    print(f"Hello {uzytkownik} ")
