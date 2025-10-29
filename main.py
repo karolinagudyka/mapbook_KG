2ca1cbdb7a9dd0aa38eb1a85306db534ac39fca6
karolinagudyka <u56121@student.wat.edu.pl>
2025-10-29 10:36:54 +0100

    Wip: added function display user info

--- main.py
+++ main.py
@@ -9,5 +9,22 @@ users:list = [
       "location": "Londyn",
       "posts": "10" },
 ]
-for user in users:
-    print(f"Twój znajomy {user["name"]} z miejscowości {user["location"]} opublikował {user["posts"]} postów")
+
+def user_info(users_data:list)->None:
+    for user in users_data:
+        print(f"Twój znajomy {user["name"]} z miejscowości {user["location"]} opublikował {user["posts"]} postów")
+
+while True:
+    tmp_choice:int=int(input("Wybierz opcję menu: "))
+    if tmp_choice==0:
+        break
+    if tmp_choice==1:
+        print("Wybrano funkcję wyświetlania aktywności znajomych")
+        user_info(users)
+    if tmp_choice==2:
+        print("Wybrano funkcję dodawania znajomego")
+    if tmp_choice==3:
+        print("Wybrano funkcję usuwania znajomego")
+    if tmp_choice==4:
+        print("Wybrano funkcję aktualizacji danych znajomego")
+
